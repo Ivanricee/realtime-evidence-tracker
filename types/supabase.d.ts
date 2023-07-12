e449186980034b48cdb7c7de3698b4c4274f61ea
Iván Bautista <ivanbautistag@gmail.com>
2023-07-12 12:44:36 -0600

    feat: supabase  types

---
+++ types/supabase.d.ts
@@ -0,0 +1,109 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      clips: {
+        Row: {
+          created_at: string | null
+          id: number
+          participant_id: number | null
+          status: string | null
+          url: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id?: number
+          participant_id?: number | null
+          status?: string | null
+          url?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: number
+          participant_id?: number | null
+          status?: string | null
+          url?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: 'clips_participant_id_fkey'
+            columns: ['participant_id']
+            referencedRelation: 'participants'
+            referencedColumns: ['id']
+          },
+        ]
+      }
+      participants: {
+        Row: {
+          created_at: string | null
+          id: number
+          imgProfile: string | null
+          name: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id?: number
+          imgProfile?: string | null
+          name?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: number
+          imgProfile?: string | null
+          name?: string | null
+        }
+        Relationships: []
+      }
+      questions: {
+        Row: {
+          created_at: string | null
+          id: number
+          participant_id: number | null
+          question: string | null
+          status: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id?: number
+          participant_id?: number | null
+          question?: string | null
+          status?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: number
+          participant_id?: number | null
+          question?: string | null
+          status?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: 'questions_participant_id_fkey'
+            columns: ['participant_id']
+            referencedRelation: 'participants'
+            referencedColumns: ['id']
+          },
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
