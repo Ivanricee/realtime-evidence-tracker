8df79dc9fd412098c2e4b21d3b1cc623758fb64c
Iván Bautista <ivanbautistag@gmail.com>
2023-07-12 19:48:39 -0600

    feat: display toaster for empty participants

--- app/participants/[id]/EvidenceForm.tsx
+++ app/participants/[id]/EvidenceForm.tsx
@@ -1,40 +1,52 @@
 'use client'
 import { Skeleton, TextField } from '@mui/material'
 import { Submit } from './Submit'
-import { useEffect, useRef, useState } from 'react'
-
+import { useEffect, useRef } from 'react'
 import { useToaster } from '@/hooks/useToaster'
-import { EVIDENCE } from '@/const'
+import { EVIDENCE, INSERT, NO_DATA, PARTICIPANTS, SELECT } from '@/const'
 import { addEvidenceAction } from '@/app/actions/addEvidenceAction'
 import { Toaster } from '@/components/Toaster'
-type Prop = {
-  id: number
-  serverParticipants: Participants[]
+
+type Props = {
+  serverParticipants: Participants[] | []
 }
-export function EvidenceForm({ id, serverParticipants }: Prop) {
+
+export function EvidenceForm({ serverParticipants }: Props) {
   const [toaster, openToaster, resetToaster] = useToaster()
-  const [participantName, setParticipantName] = useState<string | null>(null)
+
   const urlRef = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
-    serverParticipants.forEach(
-      (participant) =>
-        participant.id === id && setParticipantName(participant.name)
-    )
-  }, [id, serverParticipants])
+    if (serverParticipants.length === 0) {
+      openToaster({ feature: PARTICIPANTS, action: SELECT, status: NO_DATA })
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const submitAction = async (formData: FormData) => {
     if (urlRef.current) urlRef.current.value = ''
     const { data, status } = await addEvidenceAction(formData)
-    const toasterParams = { feature: EVIDENCE, status }
-    openToaster(toasterParams)
+    openToaster({ feature: EVIDENCE, action: INSERT, status })
   }
 
+  if (serverParticipants.length === 0) {
+    return (
+      toaster.isOpen && (
+        <Toaster
+          toaster={toaster}
+          resetToaster={resetToaster}
+          linkSrc="/dashboard"
+          linkTitle="/dashboard"
+        />
+      )
+    )
+  }
+  const participant = serverParticipants[0] as Participants
   return (
     <div className="flex w-screen flex-wrap justify-center p-4 gap-4">
-      <h1 className="text-center text-6xl">{participantName}</h1>
+      <h1 className="text-center text-6xl">{participant.name}</h1>
 
-      {!participantName && (
+      {!participant.name && (
         <span className="min-w-[150px] w-2/5">
           <Skeleton variant="text" sx={{ fontSize: '4rem' }} />
         </span>
@@ -48,7 +60,7 @@ export function EvidenceForm({ id, serverParticipants }: Prop) {
           fullWidth
           id="url"
           name="url"
-          label="Evidencia"
+          label="Url clip"
           variant="outlined"
           autoFocus
           required
@@ -56,8 +68,8 @@ export function EvidenceForm({ id, serverParticipants }: Prop) {
         <TextField
           id="participant_id"
           name="participant_id"
-          label="Evidencia"
-          defaultValue={id}
+          label="participant id"
+          defaultValue={participant.id}
           hidden
           variant="outlined"
         />
