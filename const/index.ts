86153d9cb50dbf2cb4c1b3dbe13e9d7ab037f732
Iván Bautista <ivanbautistag@gmail.com>
2023-07-12 19:45:35 -0600

    feat: key option to add type of fetch: select, update or delete

--- const/index.ts
+++ const/index.ts
@@ -1,8 +1,10 @@
 interface MessageStatus {
   [key: string]: {
-    [key: number]: {
-      message: string
-      connectionStatus: string
+    [key: symbol]: {
+      [key: number]: {
+        message: string
+        connectionStatus: string
+      }
     }
   }
 }
@@ -10,27 +12,42 @@ export const STATUS_SUCCESS = '201'
 export const STATUS_ERROR = '404'
 export const EVIDENCE = 'evidence'
 export const PARTICIPANTS = 'participants'
+export const SELECT = Symbol()
+export const INSERT = Symbol()
+export const UPDATE = Symbol()
+export const NO_DATA = 0
 export const DATABASE_STATUS = {
   [EVIDENCE]: {
-    [STATUS_SUCCESS]: {
-      message: 'Evidencia agregada correctamente, puedes revisarlo en ',
-      connectionStatus: 'success',
-    },
-    [STATUS_ERROR]: {
-      message:
-        'Ocurrio un error al intentar agregar la evidencia en la base de datos',
-      connectionStatus: 'error',
+    [INSERT]: {
+      [STATUS_SUCCESS]: {
+        message: 'Evidencia agregada correctamente, puedes revisarlo en ',
+        connectionStatus: 'success',
+      },
+      [STATUS_ERROR]: {
+        message:
+          'Ocurrio un error al intentar agregar la evidencia en la base de datos',
+        connectionStatus: 'error',
+      },
     },
   },
   [PARTICIPANTS]: {
-    [STATUS_SUCCESS]: {
-      message:
-        'Participante agregado correctamente, puedes cerrar o seguir agregando',
-      connectionStatus: 'success',
+    [INSERT]: {
+      [STATUS_SUCCESS]: {
+        message:
+          'Participante agregado correctamente, puedes cerrar o seguir agregando',
+        connectionStatus: 'success',
+      },
+      [STATUS_ERROR]: {
+        message: 'Ocurrio un error al intentar crear un nuevo participante',
+        connectionStatus: 'error',
+      },
     },
-    [STATUS_ERROR]: {
-      message: 'Ocurrio un error al intentar crear un nuevo participante',
-      connectionStatus: 'error',
+    [SELECT]: {
+      [NO_DATA]: {
+        message:
+          'No se encontraron participantes en la búsqueda realizada. revisa',
+        connectionStatus: 'info',
+      },
     },
   },
 } as MessageStatus
