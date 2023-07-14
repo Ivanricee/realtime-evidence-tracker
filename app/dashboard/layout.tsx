e6bc39e31949e4fd29e7f19eb14fb382c1d96cb8
Iván Bautista <ivanbautistag@gmail.com>
2023-07-14 00:26:59 -0600

    feat: scatter blur  lights

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -12,17 +12,26 @@ export default async function DashboardLayout({ children }: Props) {
   if (error) return <div>No se pudo obtener los participantes</div>
 
   return (
-    <section className="h-full">
-      <section className="flex gap-4 h-full">
-        <div className="w-4/12 h-full border-2 border-gray-900 rounded-lg overflow-y-auto">
-          <h1 className="text-gray-600 text-center py-8 pb-4 text-3xl">
-            Participantes
-          </h1>
-          {<ParticipantMenu serverParticipants={participants} />}
-        </div>
-        <div className="w-8/12 border-2 border-gray-900 rounded-lg">
-          {children}
-        </div>
+    <section className="h-full relative ">
+      <div
+        className="absolute aspect-video h-60 bg-green-500 blur-3xl bg-opacity-10
+      rounded-full translate-y-40 left-[10%] top-[10%]"
+      ></div>
+      <div
+        className="absolute aspect-video h-56 bg-violet-600 blur-3xl bg-opacity-20
+      rounded-full right-20 bottom-20 top-0 left-0"
+      ></div>
+      <div
+        className="absolute aspect-video h-72 bg-violet-500 blur-3xl bg-opacity-10
+      rounded-full top-[60%] left-[50%]"
+      ></div>
+      <div
+        className="absolute aspect-video h-80 bg-green-500 blur-3xl bg-opacity-10
+        rounded-full top-[30%] left-[50%]"
+      ></div>
+      <section className="flex gap-4 h-full relative z-10">
+        {<ParticipantMenu serverParticipants={participants} />}
+        <div className="w-9/12 rounded-lg">{children}</div>d
       </section>
     </section>
   )
