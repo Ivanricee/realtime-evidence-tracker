65990ee80e32c6f9b937b394be1b8642e7a7abde
Iván Bautista <ivanbautistag@gmail.com>
2023-07-14 00:26:05 -0600

    feat: add background with opacity

--- app/dashboard/[participant]/page.tsx
+++ app/dashboard/[participant]/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Box } from '@mui/material'
 import Paper from '@mui/material/Paper'
 type Prop = {
   params: { participant: string }
@@ -8,9 +9,13 @@ export default function dashboardParticipant({ params }: Prop) {
     <section aria-label="dashboard evidence" className="w-full h-full">
       <section
         aria-label="user data edition and media"
-        className="h-2/3 grid grid-cols-6 grid-rows-7 gap-2"
+        className="h-2/3 grid grid-cols-6 grid-rows-7 gap-4"
       >
-        <Paper elevation={3} className="col-span-2 row-span-6">
+        <Paper
+          className="col-span-2 row-span-6 rounded-xl
+          bg-transparent bg-gradient-to-t from-zinc-800/30 from-20% ... to-35%"
+          elevation={12}
+        >
           <h1>{params.participant}</h1>
           <p>Participant info</p>
           <small> get stats realtime participant</small>
@@ -27,15 +32,28 @@ export default function dashboardParticipant({ params }: Prop) {
           </ol>
         </Paper>
 
-        <Paper elevation={3} className="col-span-4 row-span-5 ">
+        <Paper
+          elevation={12}
+          className="col-span-4 row-span-5  rounded-xl
+          bg-transparent bg-gradient-to-t from-zinc-800/30 from-20% ... to-65%"
+        >
           Video media
         </Paper>
-        <Paper elevation={3} className="col-span-4 ">
+        <Paper
+          elevation={12}
+          className="col-span-4  rounded-xl
+          bg-transparent bg-gradient-to-t from-zinc-800/30 from-20% ... to-60%"
+        >
           buttons maybe?
         </Paper>
       </section>
-      <section aria-label="evidence list" className=" h-1/3 pt-2">
-        <Paper elevation={3} className="h-full ">
+      <section aria-label="evidence list" className=" h-1/3 pt-4">
+        <Paper
+          elevation={12}
+          className="h-full rounded-xl
+          bg-transparent bg-gradient-to-t from-zinc-800/30 from-30% ... to-70%%
+          "
+        >
           LIST OF CLIPS
         </Paper>
       </section>
