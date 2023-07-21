0e0a281abf0f378c29cc045e17f4b81c15f29eab
Iván Bautista <ivanbautistag@gmail.com>
2023-07-21 13:36:18 -0600

    feat: action server to get participant evidence view

---
+++ app/actions/getParticipantEvidences.ts
@@ -0,0 +1,25 @@
+'use server'
+
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+type Props = {
+  participantId: string
+}
+export const getParticipantEvidences = async ({ participantId }: Props) => {
+  const supabase = createServerComponentClient({ cookies })
+
+  try {
+    const { data, error } = await supabase
+      .from('participantevidenceview')
+      .select('*')
+    if (error) {
+      return { data: null, status: 404 }
+    }
+
+    return data
+  } catch (error) {
+    console.log('error de conexion ', error)
+
+    return { data: null, status: 404 }
+  }
+}
