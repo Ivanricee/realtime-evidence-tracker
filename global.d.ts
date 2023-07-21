5fef59f900401f2bdeefc37528e9f94ab2649bb8
Iván Bautista <ivanbautistag@gmail.com>
2023-07-21 13:42:26 -0600

    feat: add supabase type tables

--- global.d.ts
+++ global.d.ts
@@ -2,5 +2,10 @@ import type { Database as DB } from './types/supabase'
 
 declare global {
   type Database = DB
+  type SupabaseError = { data: null; status: number }
   type Participants = Database['public']['Tables']['participants']['Row']
+  type Evidence = Database['public']['Tables']['evidence']['Row']
+  type Questions = Database['public']['Tables']['questions']['Row']
+  type participantEvidenceView =
+    Database['public']['Views']['participantevidenceview']['Row']
 }
