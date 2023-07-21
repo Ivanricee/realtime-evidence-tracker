9f7c3855d8b4dba9c0e4334d7681220ec35b2c09
Iván Bautista <ivanbautistag@gmail.com>
2023-07-21 13:42:05 -0600

    feat: add components files to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   corePlugins: {
     preflight: true,
   },
-  content: ['./app/**/*.{js,ts,jsx,tsx,css}'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,css}',
+    './components/**/*.{js,ts,jsx,tsx,css}',
+  ],
+
   important: '#__next',
   theme: {
     extend: {},
