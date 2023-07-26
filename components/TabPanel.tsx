2651f2e656be90e7cf09e9ad0602c471bab39bc1
Iván Bautista <ivanbautistag@gmail.com>
2023-07-26 12:11:43 -0600

    refactor: remove extra html

--- components/TabPanel.tsx
+++ components/TabPanel.tsx
@@ -1,27 +1,30 @@
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
+import { Grow } from '@mui/material'
 interface TabPanelProps {
   children?: React.ReactNode
   dir?: string
   index: number
   value: number
 }
-
+export function a11yProps(index: number) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  }
+}
 export default function TabPanel(props: TabPanelProps) {
   const { children, value, index } = props
 
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
+    <Grow in={value === index}>
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`full-width-tabpanel-${index}`}
+        aria-labelledby={`full-width-tab-${index}`}
+        className="h-full"
+      >
+        {value === index && children}
+      </div>
+    </Grow>
   )
 }
