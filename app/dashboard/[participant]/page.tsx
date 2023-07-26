18d9403242ff0f491cb392337ab60c47c4194c4f
Iván Bautista <ivanbautistag@gmail.com>
2023-07-26 12:20:16 -0600

    feat: add dynamic menu tabs

--- app/dashboard/[participant]/page.tsx
+++ app/dashboard/[participant]/page.tsx
@@ -1,44 +1,53 @@
 'use client'
-import ParticipantStats from '@/components/ParticipantStats/Index'
-import { Box, Button } from '@mui/material'
-import Paper from '@mui/material/Paper'
+import TabPanel, { a11yProps } from '@/components/TabPanel'
+import AppBar from '@mui/material/AppBar'
+import { useState } from 'react'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+import { Evidence } from './Evidence'
+import {
+  DASHBOARD_MENU,
+  M_BITSSUBS,
+  M_EVIDENCIAS,
+  M_PREGUNTAS,
+  type MenuKey,
+} from '@/const'
+
 type Prop = {
   params: { participant: string }
 }
-export default function dashboardParticipant({ params }: Prop) {
-  return (
-    <section aria-label="dashboard evidence" className="w-full h-full flex">
-      <section
-        aria-label="user data edition and media"
-        className="h-full w-full flex flex-col justify-center "
-      >
-        <Paper
-          elevation={12}
-          className="rounded-xl flex flex-col h-4/6 gap-4 p-4
-          bg-transparent bg-gradient-to-t from-zinc-800/30 from-20% ... to-65%"
-        >
-          <div className="h-5/6 bg-black/60 bg-opacity-80 rounded-lg w-full flex justify-center items-center">
-            <h3 className="text-indigo-300">
-              Ve evidencias con ayuda de la lista de abajo
-            </h3>
-          </div>
-          <div className="flex rounded-xl h-1/6 w-full justify-evenly items-center">
-            <Button variant="outlined" disabled>
-              Aceptar
-            </Button>
-            <Button variant="outlined">Descartar</Button>
-          </div>
-        </Paper>
 
-        <Paper
-          elevation={12}
-          className="rounded-xl h-2/6
-          bg-transparent bg-gradient-to-t from-zinc-800/30 from-30% ... to-70%%
-          "
-        >
-          LIST OF CLIPS
-        </Paper>
-      </section>
+export default function DashboardParticipant({ params }: Prop) {
+  const [value, setValue] = useState(0)
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    console.log({ newValue })
+
+    setValue(newValue)
+  }
+  const MENU_COMPONENTS: Record<MenuKey, React.ReactNode> = {
+    [M_EVIDENCIAS]: <Evidence />,
+    [M_PREGUNTAS]: <Evidence />,
+    [M_BITSSUBS]: <Evidence />,
+  }
+  return (
+    <section aria-label="Evidence" className="w-full h-full flex flex-col">
+      <AppBar
+        position="static"
+        className="w-full rounded-lg bg-opacity-5 bg-transparent bg-gradient-to-r from-zinc-700/40 from-10% ... to-65%"
+      >
+        <Tabs value={value} onChange={handleChange} aria-label="Dashboard menu">
+          {DASHBOARD_MENU.map((menu, index) => (
+            <Tab key={menu} label={menu} {...a11yProps(index)} />
+          ))}
+        </Tabs>
+      </AppBar>
+      <div className="h-full">
+        {DASHBOARD_MENU.map((menu, index) => (
+          <TabPanel key={menu} value={value} index={index}>
+            {MENU_COMPONENTS[menu]}
+          </TabPanel>
+        ))}
+      </div>
     </section>
   )
 }
