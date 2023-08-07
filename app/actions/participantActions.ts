5248d11b29a473ee2be602a461b2d0912273e54f
Iván Bautista <ivanbautistag@gmail.com>
2023-08-07 14:06:35 -0600

    feat: new function, addParticipant and DeleteParticipant

--- app/actions/participantActions.ts
+++ app/actions/participantActions.ts
@@ -2,6 +2,27 @@
 import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
 import { revalidatePath } from 'next/cache'
 import { cookies } from 'next/headers'
+type Props = {
+  participantId: string | null
+}
+export const getParticipants = async ({ participantId }: Props) => {
+  const supabase = createServerActionClient({ cookies })
+
+  try {
+    const { data, error } = await supabase
+      .from('participants')
+      .select('name')
+      .eq('participant_id', participantId)
+    if (error) {
+      return [{ data: null, status: 404 }]
+    }
+    return data
+  } catch (error) {
+    console.log('error de conexion ', error)
+
+    return [{ data: null, status: 404 }]
+  }
+}
 
 export const addParticipantAction = async (formData: FormData) => {
   const name = formData.get('name')
@@ -36,5 +57,25 @@ export const editParticipantAction = async (formData: FormData) => {
     console.log('error de conexion particpants', error)
 
     return { data: null, status: 404 }
+  }
+}
+export const deleteParticipant = async ({ participantId }: Props) => {
+  const supabase = createServerActionClient({
+    cookies,
+  })
+
+  try {
+    const { data, status } = await supabase
+      .from('participants')
+      .delete()
+      .eq('id', participantId)
+    if (status !== 204) {
+      return [{ data: null, status: 404 }]
+    }
+    return status
+  } catch (error) {
+    console.log('error de conexion ', error)
+
+    return [{ data: null, status: 404 }]
   }
 }
