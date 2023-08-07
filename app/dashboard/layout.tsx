7f31258772de6b6dfbc553e050db2707ab3bcf8c
Iván Bautista <ivanbautistag@gmail.com>
2023-08-07 13:26:28 -0600

    refactor: Switch background from dashboard to main layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,27 +1,11 @@
-import ParticipantMenu from './ParticipantMenu'
+import ParticipantMenu from './Participant/ParticipantMenu'
 
 type Props = {
   children: React.ReactNode
 }
 export default async function DashboardLayout({ children }: Props) {
   return (
-    <section className="h-full relative">
-      <div
-        className="absolute aspect-video h-60 bg-green-500 blur-3xl bg-opacity-10
-      rounded-full translate-y-40 left-[10%] top-[10%]"
-      ></div>
-      <div
-        className="absolute aspect-video h-56 bg-violet-600 blur-3xl bg-opacity-20
-      rounded-full right-20 bottom-20 top-0 left-0"
-      ></div>
-      <div
-        className="absolute aspect-video h-72 bg-violet-500 blur-3xl bg-opacity-10
-      rounded-full top-[60%] left-[50%]"
-      ></div>
-      <div
-        className="absolute aspect-video h-80 bg-green-500 blur-3xl bg-opacity-10
-        rounded-full top-[30%] left-[50%]"
-      ></div>
+    <section className="h-full ">
       <section className="flex gap-4 h-full relative z-10">
         <section
           aria-label="evidence list"
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,23 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className="bg-none" id="__next">
-        <main className="w-full h-screen bg-cover p-6 overflow-hidden  min-h-[600px]">
+        <main className="w-full h-screen bg-cover p-6 overflow-hidden  min-h-[600px] relative">
+          <div
+            className="absolute aspect-video h-60 bg-green-500 blur-3xl bg-opacity-10
+          rounded-full translate-y-40 left-[10%] top-[10%]"
+          ></div>
+          <div
+            className="absolute aspect-video h-56 bg-violet-600 blur-3xl bg-opacity-20
+          rounded-full right-20 bottom-20 top-0 left-0"
+          ></div>
+          <div
+            className="absolute aspect-video h-72 bg-violet-500 blur-3xl bg-opacity-10
+          rounded-full top-[60%] left-[50%]"
+          ></div>
+          <div
+            className="absolute aspect-video h-80 bg-green-500 blur-3xl bg-opacity-10
+            rounded-full top-[30%] left-[50%]"
+          ></div>
           <ThemeRegistry>{children}</ThemeRegistry>
         </main>
       </body>
