bdbed9760fb172302f2a44c3e157adea4387c505
Iván Bautista <ivanbautistag@gmail.com>
2023-08-14 21:57:55 -0600

    feat:update supabase types

--- global.d.ts
+++ global.d.ts
@@ -5,9 +5,12 @@ declare global {
   type SupabaseError = { data: null; status: number }
   type Participants = Database['public']['Tables']['participants']['Row']
   type Evidence = Database['public']['Tables']['evidence']['Row']
-  type Questions = Database['public']['Tables']['questions']['Row']
+  type Questions = Database['public']['Tables']['quizz']['Row']
+  type Sancion = Database['public']['Tables']['sancion']['Row']
   type participantEvidenceView =
     Database['public']['Views']['participantevidenceview']['Row']
   type participantsancionview =
     Database['public']['Views']['participantsancionview']['Row']
+  type pendingSancionView =
+    Database['public']['Views']['participantpendingsancionview']['Row']
 }
