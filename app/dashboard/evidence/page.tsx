2a3892f72ead53962e288998f7d5ae5548802d65
Iván Bautista <ivanbautistag@gmail.com>
2023-08-15 20:26:55 -0600

    feat: init message for route

---
+++ app/dashboard/evidence/page.tsx
@@ -0,0 +1,10 @@
+export default function Evidence() {
+  return (
+    <div
+      className="rounded-xl shadow-2xl grid place-content-center h-full gap-4 p-4  box-border
+      bg-transparent bg-gradient-to-t from-zinc-800/20 from-20% to-65%"
+    >
+      <h1>Selecciona un participante de la izquierda</h1>
+    </div>
+  )
+}
---
+++ app/dashboard/quiz/page.tsx
@@ -0,0 +1,10 @@
+export default function Quiz() {
+  return (
+    <div
+      className="rounded-xl shadow-2xl grid place-content-center h-full gap-4 p-4  box-border
+  bg-transparent bg-gradient-to-t from-zinc-800/20 from-20% to-65%"
+    >
+      <h1>Quiz</h1>
+    </div>
+  )
+}
