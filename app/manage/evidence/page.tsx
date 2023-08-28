c25ae3aad1b093243e9ede21eb478c0b8d555963
Iván Bautista <ivanbautistag@gmail.com>
2023-08-28 13:27:01 -0600

    refactor: split code

--- app/manage/evidence/page.tsx
+++ app/manage/evidence/page.tsx
@@ -1,23 +1,18 @@
 'use client'
 
-import { useParticipants } from '@/hooks/useParticipants'
-import { AutocompleteParticipant } from './AutocompleteEvidence'
-import { useAlertToast } from '@/hooks/useAlertToast'
-import { AlertToast } from '@/components/AlertToast'
-import { useEffect, useState } from 'react'
-import { E_FULFILLED, NO_DATA, PARTICIPANTS, SELECT } from '@/const'
+import { useState } from 'react'
+import { useSearchParams } from 'next/navigation'
 import { Grow, Skeleton } from '@mui/material'
 import Divider from '@mui/material/Divider'
-import { useSearchParams } from 'next/navigation'
-import { EvidenceForm } from './EvidenceForm'
 import Button from '@mui/material/Button'
+import { E_FULFILLED } from '@/const'
+import { AutocompleteParticipant } from '../../../components/AutocompleteParticipants'
+import { EvidenceForm } from './EvidenceForm'
 import { useRealtimeParticipantSancion } from '@/hooks/useRealtimeParticipantSancion'
 import { useSanctionEdit } from '@/hooks/useSanctionEdit'
-import { Toaster } from 'sonner'
 
 export default function Evidence() {
   const [participant, setParticipant] = useState<Participants | null>(null)
-  const [participants, loading, error] = useParticipants()
   const [editSanctionStatus, loadingSanction] = useSanctionEdit()
   const [sancion, loadingSancion, errorSancion] = useRealtimeParticipantSancion(
     {
@@ -25,61 +20,13 @@ export default function Evidence() {
       isSingleRow: true,
     }
   )
-  const [alertToast, openAlertToast, resetAlertToast] = useAlertToast()
   const searchParams = useSearchParams()
   const participantId = searchParams.get('participantId')
 
-  const handleAutocomplete = (participants: Participants) => {
-    if (participants) setParticipant({ ...participants })
+  const handleAutocomplete = (participant: Participants) => {
+    if (participant) setParticipant({ ...participant })
   }
-  useEffect(() => {
-    //Display an alert for errors or empty data.
-    if (!loading) {
-      let status = null
-      if (participants?.length === 0) status = NO_DATA
-      if (error) status = error.status
-      if (status !== null) {
-        openAlertToast({
-          feature: PARTICIPANTS,
-          action: SELECT,
-          status,
-        })
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [participants, loading])
 
-  if (loading)
-    return (
-      <>
-        <div className=" w-5/12">
-          <Skeleton variant="text" className="w-full h-20" />
-        </div>
-        {participantId && (
-          <>
-            <Divider flexItem />
-            <div className="flex flex-col w-full items-center gap-0">
-              <Skeleton variant="text" className="w-3/5 h-20 " />
-              <Skeleton variant="text" className="w-full h-20" />
-              <Skeleton variant="text" className="w-1/5 h-16 " />
-            </div>
-          </>
-        )}
-      </>
-    )
-
-  if (alertToast.isOpen)
-    return (
-      <>
-        <div className="flex justify-center px-6 mt-6">
-          <AlertToast
-            alertToast={alertToast}
-            resetAlertToast={resetAlertToast}
-            width="6/6"
-          />
-        </div>
-      </>
-    )
   return (
     <>
       {sancion[0] && (
@@ -120,9 +67,8 @@ export default function Evidence() {
       )}
       <AutocompleteParticipant
         participantId={participantId}
-        participants={participants}
         participant={participant}
-        handleAutocomplete={handleAutocomplete}
+        onChange={handleAutocomplete}
       />
       {participant?.id && (
         <Grow in={Boolean(participant?.id)} className="w-full">
@@ -132,7 +78,19 @@ export default function Evidence() {
           </div>
         </Grow>
       )}
-      <Toaster position="bottom-left" richColors closeButton />
+      {
+        //skeleton for initial participant
+        participantId && !participant && (
+          <>
+            <Divider flexItem />
+            <div className="flex flex-col w-full items-center gap-0">
+              <Skeleton variant="text" className="w-3/5 h-20 " />
+              <Skeleton variant="text" className="w-full h-20" />
+              <Skeleton variant="text" className="w-1/5 h-16 " />
+            </div>
+          </>
+        )
+      }
     </>
   )
 }
