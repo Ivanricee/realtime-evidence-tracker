7c42fdc7d4074935d4fda82de6575c0a4495a0d6
Iván Bautista <ivanbautistag@gmail.com>
2023-08-28 13:21:06 -0600

    feat: hook to get participantQuiz

---
+++ hooks/useParticipantQuiz.ts
@@ -0,0 +1,32 @@
+import { getParticipantQuizByid } from '@/app/actions/quizActions'
+import { useState } from 'react'
+
+type response = [Participants[], boolean, SupabaseError | null]
+
+export function useParticipantQuiz(): response {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<SupabaseError | null>(null)
+  const [loadingEdit, setLoadingEdit] = useState(false)
+  const [errorEdit, setErrorEdit] = useState<SupabaseError | null>(null)
+  const [participantQuiz, setParticipantQuiz] = useState<Participants[] | []>(
+    []
+  )
+
+  const getParticipantQuiz = async ({ ids }: { ids: [string] }) => {
+    setLoading(true)
+    if (error !== null) setError(null)
+    const response = await getParticipantQuizByid({ ids })
+    console.log('participan hook ', response)
+
+    if (response[0]?.data === null) {
+      setError(response[0])
+    } else {
+      setParticipantQuiz(response)
+    }
+    setLoading(false)
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }
+  //const updateParticipantQuiz = async ({ ids }: { ids: [string] }) => {}
+  return [participantQuiz, loading, error, getParticipantQuiz]
+}
