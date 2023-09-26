a6cf39a41f3e48eb4a0540699a2e4440d6a34f34
Iván Bautista <ivanbautistag@gmail.com>
2023-09-26 14:19:48 -0600

    refactor: remove comments and unused imports

--- app/actions/getParticipantSancion.tsx
+++ app/actions/getParticipantSancion.tsx
@@ -1,21 +1,27 @@
 'use server'
 
+import { EVIDENCE } from '@/const'
 import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 type Props = {
   participantId: string | null
+  sancionType: string | null
 }
-export const getParticipantSancion = async ({ participantId }: Props) => {
+export const getParticipantSancion = async ({
+  participantId,
+  sancionType,
+}: Props) => {
   const supabase = createServerActionClient({ cookies })
-
   try {
     let query = supabase.from('participantsancionview').select('*')
 
+    //if (sancionType && sancionType === EVIDENCE) query = query.eq('')
     if (participantId) query = query.eq('id', participantId)
     const { data, error } = await query
     if (error) {
       return [{ data: null, status: 404 }]
     }
+
     return data
   } catch (error) {
     console.log('error de conexion ', error)
