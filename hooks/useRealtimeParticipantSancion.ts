13719acb3bd2e1a41a0dba425671fdf1aefd67f7
Iván Bautista <ivanbautistag@gmail.com>
2023-09-26 14:48:19 -0600

    feat: debounce function to get sanction count every second

--- hooks/useRealtimeParticipantSancion.ts
+++ hooks/useRealtimeParticipantSancion.ts
@@ -1,19 +1,25 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { getParticipantSancion } from '@/app/actions/getParticipantSancion'
+import { debounce } from '@/utils'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 type responseHook = [participantsancionview[], boolean, SupabaseError | null]
 type Props = {
   participantId: string | null
+  sancionType?: string | null
   isSingleRow?: boolean
 }
+//issingleRow for when participantId is not needed
 export function useRealtimeParticipantSancion({
   participantId,
+  sancionType = null,
   isSingleRow = false,
 }: Props): responseHook {
   const supabase = createClientComponentClient<Database>()
 
   const [loading, setLoading] = useState(true)
+  const [debounceLoading, setDebounceLoading] = useState(false)
   const [error, setError] = useState<SupabaseError | null>(null)
   const [particSancion, setParticSancion] = useState<
     participantsancionview[] | []
@@ -24,7 +30,10 @@ export function useRealtimeParticipantSancion({
       if (!loading) setLoading(true)
 
       const participantSancion = async () => {
-        const response = await getParticipantSancion({ participantId })
+        const response = await getParticipantSancion({
+          participantId,
+          sancionType,
+        })
         if (response[0]?.data === null) {
           setError(response[0])
         } else {
@@ -34,11 +43,35 @@ export function useRealtimeParticipantSancion({
       }
       participantSancion()
     }
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [participantId])
 
   //subscript to sancion then update local state
+  const debounceUpdate = useCallback(() => {
+    const callback = async () => {
+      if (!loading) setLoading(true)
+
+      const participantSancion = async () => {
+        const response = await getParticipantSancion({
+          participantId,
+          sancionType,
+        })
+        if (response[0]?.data === null) {
+          setError(response[0])
+        } else {
+          setParticSancion(response)
+        }
+        await setLoading(false)
+        setDebounceLoading(false)
+      }
+      participantSancion()
+    }
+    const delay = 1000
+    const debouncedFn = debounce({ callback, delay })
+    setDebounceLoading(true)
+    debouncedFn()
+  }, [participantId])
+
   useEffect(() => {
     const channel = supabase
       .channel('sancion')
@@ -55,11 +88,7 @@ export function useRealtimeParticipantSancion({
             Number(participantId) === newParticipantId ||
             payload.eventType === 'DELETE'
           ) {
-            const participantSancion = async () => {
-              const response = await getParticipantSancion({ participantId })
-              if (!response[0]?.error) setParticSancion(response)
-            }
-            participantSancion()
+            debounceUpdate()
           }
         }
       )
@@ -69,5 +98,6 @@ export function useRealtimeParticipantSancion({
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [supabase, participantId])
-  return [particSancion, loading, error]
+  const isLoading = loading || debounceLoading
+  return [particSancion, isLoading, error]
 }
