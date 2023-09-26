4dbecc83db3a520a9cedcd210cdbc243ef76f58d
Iván Bautista <ivanbautistag@gmail.com>
2023-09-26 14:21:08 -0600

    feat: add filter status: pending & type evidence or participant_quiz id

--- app/actions/sanctionActions.ts
+++ app/actions/sanctionActions.ts
@@ -1,20 +1,31 @@
 'use server'
+import { EVIDENCE, QUIZ } from '@/const'
 import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 type EditProps = {
   id: number
   status: string
+  type: string
 }
-export const editSancion = async ({ id, status }: EditProps) => {
+
+export const editSancion = async ({ id, status, type }: EditProps) => {
   const supabase = createServerActionClient({ cookies })
   try {
-    const result = await supabase
+    let query = supabase
       .from('sancion')
       .update({ status: status })
       .eq('participant_id', id)
+      .eq('status', 'pending')
+
+    if (type === EVIDENCE) query = query.not('evidence_id', 'is', null)
+    if (type === QUIZ) query = query.not('participant_quiz_id', 'is', null)
+
+    const result = await query
+
     if (result.error) {
       return [{ data: null, status: 404 }]
     }
+
     //204
     return result.status
   } catch (error) {
