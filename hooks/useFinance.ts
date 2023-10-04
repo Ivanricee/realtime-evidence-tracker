b9bcfe9191e318bc8dbe61879c7828bb10479bd0
Iván Bautista <ivanbautistag@gmail.com>
2023-10-04 17:53:34 -0600

    feat: subscribe, get and cahce data from finance and useSwr

--- hooks/useFinance.ts
+++ hooks/useFinance.ts
@@ -1,66 +1,55 @@
-import { getParticipantQuizByid } from '@/app/actions/quizActions'
+import { getFinance } from '@/app/actions/financeActions'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { RealtimeChannel } from '@supabase/supabase-js'
 
 import { useEffect, useState } from 'react'
+import useSWR, { mutate } from 'swr'
 
 //type getParticipantQuizType = ({ ids }: getFinanceT) => void
 
-type response = [
-  quizResultsView[],
-  boolean,
-  SupabaseError | null,
-  //getParticipantQuizType,
-]
+type response = [financeType[], boolean, SupabaseError | null]
 
 interface props {
   fromParticipantId?: null | number
 }
-export function useFinance({ fromParticipantId = null }: props) /* :response*/ {
-  const supabase = createClientComponentClient()
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState<SupabaseError | null>(null)
-  const [finance, setFinance] = useState<financeType[] | []>([])
 
-  /*const getFinance = async () => {
-    const response = await getParticipantQuizByid({ ids, participantId })
-    //console.log('response server ', response)
-
-    if (response[0]?.data === null) {
-      setError(response[0])
-    } else {
-      setParticipantQuiz(response)
-    }
-    setLoading(false)
+const fetcher = async ({ fromParticipantId }: props) => {
+  const participantId = fromParticipantId
+  const response = participantId ? await getFinance({ participantId }) : []
+  if (response[0]?.data === null) {
+    throw new Error('failed to fetch participants')
   }
+
+  return response
+}
+
+export function useFinance({ fromParticipantId = null }: props): response {
+  const supabase = createClientComponentClient()
+  const { data: finance, error } = useSWR<financeType[]>(
+    ['finance-key', fromParticipantId],
+    () => fetcher({ fromParticipantId })
+  )
+
   useEffect(() => {
-    let filter = {}
-    let channelPQ: RealtimeChannel | null = null
-    if (participantId) {
-      filter = { filter: `participant_id=eq.${participantId}` }
+    const channel = supabase
+      .channel('finance')
+      .on(
+        'postgres_changes',
+        {
+          event: '*',
+          schema: 'public',
+          table: 'finance',
+        },
+        (payload) => {
+          mutate(['finance-key', fromParticipantId])
+        }
+      )
+      .subscribe()
 
-      channelPQ = supabase
-        .channel('pq-insert-channel')
-        .on(
-          'postgres_changes',
-          {
-            event: 'INSERT',
-            schema: 'public',
-            table: 'participantQuiz',
-            ...filter,
-          },
-          (payload) => {
-            const ids: number[] = [Number(payload.new.id)]
-            getParticipantQuiz({ ids })
-          }
-        )
-        .subscribe()
-    }
     return () => {
-      if (channelPQ instanceof RealtimeChannel)
-        supabase.removeChannel(channelPQ)
+      supabase.removeChannel(channel)
     }
-  }, [participantId, supabase])*/
 
-  // return [participantQuiz, loading, error, getParticipantQuiz]*/
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [supabase, fromParticipantId])
+  return [finance || [], !error && !finance, error]
 }
