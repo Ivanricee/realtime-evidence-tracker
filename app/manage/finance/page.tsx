e87b57791d1a662b27cf2c50543b5d7d1872d228
Iván Bautista <ivanbautistag@gmail.com>
2023-10-04 17:55:02 -0600

    feat: main page for finance

--- app/manage/finance/page.tsx
+++ app/manage/finance/page.tsx
@@ -1,9 +1,32 @@
+'use client'
+import { AutocompleteParticipant } from '@/components/AutocompleteParticipants'
+import { useSearchParams } from 'next/navigation'
+import { useState } from 'react'
+import ListFinanceDetails from './ListFinanceDetails'
+
 export default function Financial() {
+  const [participant, setParticipant] = useState<Participants | null>(null)
+  const searchParams = useSearchParams()
+  const participantId = searchParams.get('participantId')
+
+  const handleAutocomplete = (participant: Participants) => {
+    if (participant) setParticipant({ ...participant })
+  }
+
   return (
-    <div className="pt-8 grid place-items-center overflow-y-auto h-full w-full">
+    <section className="pt-8 grid place-items-center overflow-y-auto h-full w-full">
       <div className="w-full flex flex-col items-center gap-8 ">
-        <h1>Bits & subs</h1>
+        <div className="min-w-[16rem]">
+          <AutocompleteParticipant
+            participantId={participantId}
+            participant={participant}
+            onChange={handleAutocomplete}
+          />
+        </div>
+        <div className="w-full">
+          {participant && <ListFinanceDetails participant={participant} />}
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
