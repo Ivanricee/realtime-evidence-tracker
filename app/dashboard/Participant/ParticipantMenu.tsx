7d679e6fc9b924f4a1e0e6b0e5ad04478879ca76
Iván Bautista <ivanbautistag@gmail.com>
2023-10-18 20:51:53 -0600

    feat: menu inside Dialog for md breakpoint

--- app/dashboard/Participant/ParticipantMenu.tsx
+++ app/dashboard/Participant/ParticipantMenu.tsx
@@ -1,16 +1,70 @@
 'use client'
 import { useRealtimeParticipantEvidence } from '@/hooks/useRealtimeParticipantEvidence'
-import { Divider, List, Paper, Skeleton } from '@mui/material'
+import {
+  AppBar,
+  Button,
+  Dialog,
+  Divider,
+  IconButton,
+  List,
+  Paper,
+  Skeleton,
+  Toolbar,
+  Typography,
+  useTheme,
+} from '@mui/material'
+import useMediaQuery from '@mui/material/useMediaQuery'
 import AddParticipant from './AddParticipant'
 import { useParams } from 'next/navigation'
 import { useAlertToast } from '@/hooks/useAlertToast'
 import { AlertToast } from '@/components/AlertToast'
 import { NO_DATA, PARTICIPANTS, SELECT } from '@/const'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { ParticipantMenuItem } from './ParticipantMenuItem'
+import CloseIcon from '@mui/icons-material/Close'
 
 type Card = {
   children: React.ReactNode
+}
+interface dialog {
+  children: React.ReactNode
+  open: boolean
+  handleClose: () => void
+}
+function DialogMenu({ children, open, handleClose }: dialog) {
+  const theme = useTheme()
+  return (
+    <Dialog
+      fullScreen
+      open={open}
+      onClose={handleClose}
+      className="md:hidden"
+      PaperProps={{
+        style: { backgroundColor: theme.palette.background.default },
+      }}
+      //TransitionComponent={Transition}
+    >
+      <AppBar sx={{ position: 'relative' }}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            onClick={handleClose}
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>
+          <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+            Participantes
+          </Typography>
+          <Button autoFocus color="inherit" onClick={handleClose}>
+            Cerrar
+          </Button>
+        </Toolbar>
+      </AppBar>
+      {children}
+    </Dialog>
+  )
 }
 function Card({ children }: Card) {
   return (
@@ -47,7 +101,7 @@ function SkeletonList() {
     </div>
   )
 }
-export default function ParticipantMenu() {
+function Participants() {
   const { participant: participantId } = useParams()
   const [alertToast, openAlertToast, resetAlertToast] = useAlertToast()
   const [particEvidence, loading, error] = useRealtimeParticipantEvidence()
@@ -91,12 +145,12 @@ export default function ParticipantMenu() {
 
   return (
     <>
-      {/*participantId && <ParticipantStats />*/}
       <Card>
         <h1 className="text-purple-300/60 text-center py-6 pb-2 text-2xl">
           Participantes
         </h1>
         <AddParticipant />
+
         <Divider />
         <List component="nav" aria-label="participant menu">
           {particEvidence?.map((participant) => {
@@ -109,6 +163,29 @@ export default function ParticipantMenu() {
           })}
         </List>
       </Card>
+    </>
+  )
+}
+
+export default function ParticipantMenu() {
+  const [open, setOpen] = useState(false)
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.up('md'))
+  const handleOpen = () => {
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
+  if (matches) return <Participants />
+  return (
+    <>
+      <Button variant="outlined" onClick={handleOpen} className="md:hidden">
+        Participantes
+      </Button>
+      <DialogMenu open={open} handleClose={handleClose}>
+        <Participants />
+      </DialogMenu>
     </>
   )
 }
